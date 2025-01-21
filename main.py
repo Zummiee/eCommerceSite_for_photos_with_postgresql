09008d78b8389c0a93ebec6d8f15810c16763484
zummi <zummi@Zummie.com>
2025-01-21 20:35:49 +0000

    updated DOMAIN

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from forms import LoginForm, RegisterForm, NewProductForm, CommentForm, CheckOut
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 stripe.api_key = os.environ.get('STRIPE_API_KEY')
-SITE_DOMAIN = 'https://ecommercesite-for-photos.onrender.com'
+SITE_DOMAIN = 'https://e-commerce-site-for-photos-1gf5rzuh7-zummiees-projects.vercel.app'
 # Configure Flask-Login
 login_manager = LoginManager()
 login_manager.init_app(app)
